117ae3a05896f3be91788c084e3ecf1759accbab
Vinicius Isola <viniciusisola@gmail.com>
2021-01-02 10:55:13 -0500

    Handle process terminate signal

--- main.py
+++ main.py
@@ -1,72 +1,60 @@
 import logging
 import RPi.GPIO as GPIO
-import subprocess
+import signal
+import sys
 import time
 
 from pydispatch import dispatcher
 
-from robot import buttons
-from robot import leds
-from robot import sensors
-from robot import wheels
+import controllers
+import drivers
+import logger
 
-green_button_pressed_at = 0
-green_button_pressed_count = 0
-
-LOG_FORMAT='%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 LOGGER = logging.getLogger("robot-car-main")
 
-def green_button_pressed(pressed):
-    global green_button_pressed_at
-    global green_button_pressed_count
-    if pressed:
-        green_button_pressed_count += 1
-        dispatcher.send(signal="/led/white", on=False)
-        green_button_pressed_at = time.time()
-    else:
-        dispatcher.send(signal="/led/white", on=True)
-        green_button_pressed_at = 0
+terminating = False
+def terminate(signum=None, frame=None):
+    global terminating
+    if terminating:
+        return
+
+    terminating = True
+    if signum is not None:
+        LOGGER.info("Received signal: %d", signum)
+    LOGGER.info("Exiting...")
+    LOGGER.info("Cleaning up GPIO...")
+    GPIO.cleanup()
+    LOGGER.info("Sensors done.")
+    drivers.cleanup()
+    sys.exit(0)
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, terminate)
     LOGGER.info("Initializing robot...")
     GPIO.setmode(GPIO.BCM)
-    buttons.initialize()
-    leds.initialize()
-    sensors.initialize()
-    wheels.initialize()
+    drivers.initialize()
+    controllers.initialize()
 
-    buttons.add_green_button_handler(green_button_pressed)
     dispatcher.send(signal="/led/white", on=True)
 
-    started_robot_count = 0
-
+    # started_robot_count = 0
     try:
+        x = 0
         while True:
-            if green_button_pressed_at != 0 and time.time() - green_button_pressed_at > 5:
-                LOGGER.info("5s passed... shutting down...")
-                wheels.stop()
-                dispatcher.send(signal="/led/white", on=False)
-                subprocess.run(["sudo", "shutdown", "-h", "now"], capture_output=True)
-                break
+            x += 1
+            time.sleep(1)
+            # if started_robot_count != green_button_pressed_count:
+            #     started_robot_count = green_button_pressed_count
+            #     if wheels.current_state == wheels.STATE_STOPPED:
+            #         LOGGER.info("Moving forward...")
+            #         wheels.move_forward()
+            #     else:
+            #         LOGGER.info("Stopping...")
+            #         wheels.stop()
 
-            if started_robot_count != green_button_pressed_count:
-                started_robot_count = green_button_pressed_count
-                if wheels.current_state == wheels.STATE_STOPPED:
-                    LOGGER.info("Moving forward...")
-                    wheels.move_forward()
-                else:
-                    LOGGER.info("Stopping...")
-                    wheels.stop()
-
-            time.sleep(0.1)
+            # time.sleep(0.1)
 
     except KeyboardInterrupt:
         LOGGER.info("Interrupted...")
     finally:
-        LOGGER.info("Exiting...")
-        LOGGER.info("Cleaning up GPIO...")
-        GPIO.cleanup()
-        LOGGER.info("Sensors done.")
-
-        wheels.cleanup()
+        terminate()
